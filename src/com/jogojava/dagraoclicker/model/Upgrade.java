085f4b8f647f49946d021fe1137b19e3051ce67d
JoseVitorG <jose.viitor2109@gmail.com>
2025-07-28 19:05:53 -0300

    mudança de valores

--- src/com/jogojava/dagraoclicker/model/Upgrade.java
+++ src/com/jogojava/dagraoclicker/model/Upgrade.java
@@ -3,10 +3,10 @@ package com.jogojava.dagraoclicker.model;
 public enum Upgrade {
     ESPADA("Espada", 10, 0.1, "imgs/espada.png"),
     GUERREIRO("Guerreiro", 100, 1, "imgs/guerreiro.png"),
-    ARQUEIRO("Arqueiro", 100, 10, "imgs/arquero.png"),
-    BRUXA("Bruxa", 120, 100, "imgs/bruxa.png"),
-    GIGANTE_DE_MADEIRA("Gigante de Madeira", 150, 1000, "imgs/arvere_somos_nois.png"),
-    O_ESCOLHIDO("O Escolhido", 200, 10000, "imgs/oEscolhido.png");
+    ARQUEIRO("Arqueiro", 250, 10, "imgs/arquero.png"),
+    BRUXA("Bruxa", 2000, 100, "imgs/bruxa.png"),
+    GIGANTE_DE_MADEIRA("Gigante de Madeira", 10000, 1000, "imgs/arvere_somos_nois.png"),
+    O_ESCOLHIDO("O Escolhido", 100000, 10000, "imgs/oEscolhido.png");
 
     private final String nome;
     private final int custo;
